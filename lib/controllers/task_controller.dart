42656eebb5b91a37e319c22bf03ef3117ce4dfa3
Khoa <vinhkhoa164@gmail.com>
2023-03-31 21:58:26 +0700

    sua task

--- lib/controllers/task_controller.dart
+++ lib/controllers/task_controller.dart
@@ -8,11 +8,12 @@ class TaskController extends GetxController {
     super.onReady();
   }
 
-  v
-ar taskList = <Task>[].obs;
+  var taskList = <Task>[].obs;
+
   Future<int> addTask({Task? task}) async {
     return await DBHelper.insert(task);
   }
+
   void getTasks() async {
     List<Map<String, dynamic>> tasks = await DBHelper.query();
     taskList.assignAll(tasks.map((data) => new Task.fromJson(data)).toList());
@@ -28,4 +29,3 @@ ar taskList = <Task>[].obs;
     getTasks();
   }
 }
-dsfcsdfsdfsdfdsfsdfds
